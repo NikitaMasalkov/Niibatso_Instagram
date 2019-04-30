ded73620515aab0fcde368255409a2f0d5b27c46
nikitamasalkov <nikitamasalkov.tang@gmail.com>
2019-04-30 15:12:57 +0300

    ::JWT

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,22 @@
+class AuthenticationController < ApplicationController
+
+  def login
+    @user = User.find_by_first_name(params[:first_name])
+    if @user&.authenticate(params[:password])
+      token = ::JsonWebToken.encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                     username: @user.first_name }, status: :ok
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def login_params
+    params.permit(:first_name, :password)
+  end
+end
+
+end
